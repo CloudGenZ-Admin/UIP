a677cbb15f0912232043c218246106254a0b8c9b
Dhruvil Shah <dhruvil7797@gmail.com>
2026-04-10 12:30:36 -0700

    Fix spacing in Navbar link for 'Our Partners'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Stories", path: "/stories" },
-    { name: "OurPartners", path: "/ourpartners" },
+    { name: "Our Partners", path: "/ourpartners" },
   ];
 
   // Programs ko yahan simple link ki tarah add kiya hai
