1dc40e789b9d25f2576641688effec07389a7798
prafull dukhi <prafuldukhi@gmail.com>
2026-05-05 17:25:17 +0530

    APIupdate

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://mintcream-buffalo-179597.hostingersite.com/api'; 
-const API_URL = 'http://localhost:5000/api'; // For local development
+const API_URL = 'https://mintcream-buffalo-179597.hostingersite.com/api'; 
+// const API_URL = 'http://localhost:5000/api'; // For local development
 const apiClient = axios.create({
   baseURL: API_URL,
 });
